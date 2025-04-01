a3e69369cff43be0da524b1cbab0ea75180bf895
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-03-31 22:27:14 -0400

    Commit Linq and Context file creation

---
+++ BusinessLayer/ExpressTourDataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class DatabaseService
+    {
+        private ExpressTourDataContext db; // Instancia del contexto
+
+        public DatabaseService()
+        {
+            db = new ExpressTourDataContext(); // Se conecta automáticamente usando la cadena de conexión del .dbml
+        }
+
+        public void TestConexion()
+        {
+            try
+            {
+                db.Connection.Open();  // Abre la conexión manualmente
+                Console.WriteLine("Conexión exitosa.");
+                db.Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexión: " + ex.Message);
+            }
+        }
+    }
+}
