c661ec38efe2ec633b24e770b20386653761e929
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-02 19:09:09 -0400

    commit

---
+++ ModelLayer/DTO/ClientModelView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO
+{
+    class ClientModelView
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+
+
+    }
+}
