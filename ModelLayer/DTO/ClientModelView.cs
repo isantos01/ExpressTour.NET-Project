1027fc8e4f4fcbc8762f5005ff3f0ee3d5e6d8ad
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-02 19:42:16 -0400

    commit

--- ModelLayer/DTO/ClientModelView.cs
+++ ModelLayer/DTO/ClientModelView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ModelLayer.DTO
 {
-    class ClientModelView
+    public class ClientModelView
     {
         public int IdCliente { get; set; }
         public string Nombre { get; set; }
---
+++ ModelLayer/DTO/DashboardModelView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO
+{
+    public class DashboardModelView
+    {
+        public string Title { get; set; }
+        public string Welcome { get; set; }
+
+        public DashboardModelView()
+        {
+        }
+        public DashboardModelView(string title, string welcome)
+        {
+            Title = title;
+            Welcome = welcome;
+        }
+    }
+}
