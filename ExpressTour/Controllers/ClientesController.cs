d4dfd61edbb6fd74c37685582be6a2e8f3d82814
Ismael De Los Santos <idelossantos4@.est.unibe.do>
2025-04-07 01:52:36 -0400

    commit

--- ExpressTour/Controllers/ClientesController.cs
+++ ExpressTour/Controllers/ClientesController.cs
@@ -35,6 +35,75 @@ namespace ExpressTour.Controllers
 
             return View(clientes);
         }
+        // GET: Clientes/Create - Esta acción puede no usarse si el formulario está en el modal, pero es buena práctica tenerla.
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClientViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Mapear de ClientViewModel a la entidad cliente
+                var nuevoCliente = new cliente
+                {
+                    nombre = model.Nombre,
+                    correo = model.Correo,
+                    telefono = model.Telefono,
+                    direccion = model.Direccion
+                };
+
+                _clienteService.AgregarCliente(nuevoCliente);
+                return RedirectToAction("Index");
+            }
+            // Si hay errores, se podría regresar la vista y mostrar mensajes (o manejar via Ajax)
+            return View(model);
+        }
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var clienteEntity = _clienteService.ObtenerClientePorId(id);
+            if (clienteEntity == null)
+            {
+                return HttpNotFound();
+            }
+            var model = new ClientViewModel
+            {
+                Id = clienteEntity.id_cliente,
+                Nombre = clienteEntity.nombre,
+                Correo = clienteEntity.correo,
+                Telefono = clienteEntity.telefono,
+                Direccion = clienteEntity.direccion
+            };
+            // Devuelve el formulario de edición en un PartialView
+            return PartialView("_EditClient", model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClientViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var clienteToUpdate = new cliente
+                {
+                    id_cliente = model.Id,
+                    nombre = model.Nombre,
+                    correo = model.Correo,
+                    telefono = model.Telefono,
+                    direccion = model.Direccion
+                };
+
+                _clienteService.ActualizarCliente(clienteToUpdate);
+                return RedirectToAction("Index");
+            }
+            // Si hay errores, devuelve el PartialView con el modelo para corregirlos
+            return PartialView("_EditClient", model);
+        }
+
 
         protected override void Dispose(bool disposing)
         {
